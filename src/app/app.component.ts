4e34f0bd95ee5855f0bfa28ba986ee625c653dbf
Gérald <gerald.even@gmail.com>
2022-05-24 23:00:41 +0200

    add items and enchants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,31 @@ import {CommandComponent} from "./command/command.component";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  private static ARMOR_ENCHANTS = ['mending', 'unbreaking', 'blast_protection', 'fire_protection',
+    'thorns','projectile_protection','protection']
+  private static ARMOR_MATERIALS = ['leather', 'iron', 'gold', 'diamond', 'netherite']
+  private static TOOLS_MATERIALS = ['leather', 'iron', 'gold', 'diamond', 'netherite']
   title = 'mcgen-commands';
   public tools: Tool[] = [
     new Tool('stick').setCompatibleWith(['mending', 'unbreaking']),
-    new Tool('sword').setCompatibleWith(['mending', 'wood', 'stone', 'iron',
-      'gold', 'diamond', 'netherite', 'unbreaking', 'sharpness', 'looting']),
-    new Tool('axe').setCompatibleWith(['mending', 'wood', 'stone', 'iron',
-      'gold', 'diamond', 'netherite', 'unbreaking', 'sharpness', 'looting']),
-    new Tool('pickaxe').setCompatibleWith(['mending', 'wood', 'stone', 'iron',
-      'gold', 'diamond', 'netherite', 'unbreaking', 'sharpness', 'looting']),
-    new Tool('helmet').setCompatibleWith(['mending', 'leather', 'iron',
-      'gold', 'diamond', 'netherite', 'unbreaking', 'blast_protection', 'fire_protection','thorns'])
+    new Tool('sword').setCompatibleWith(['mending', 'unbreaking', 'sharpness', 'fire_aspect','looting','knockback'])
+      .addCompatibleWith(...AppComponent.TOOLS_MATERIALS),
+    new Tool('axe').setCompatibleWith(['mending', 'unbreaking', 'sharpness', 'looting'])
+      .addCompatibleWith(...AppComponent.TOOLS_MATERIALS),
+    new Tool('pickaxe').setCompatibleWith(['mending', 'unbreaking', 'sharpness', 'looting'])
+      .addCompatibleWith(...AppComponent.TOOLS_MATERIALS),
+    new Tool('helmet')
+      .addCompatibleWith(...AppComponent.ARMOR_MATERIALS)
+      .addCompatibleWith(...AppComponent.ARMOR_ENCHANTS,'respiration'),
+    new Tool('chestplate')
+      .addCompatibleWith(...AppComponent.ARMOR_MATERIALS)
+      .addCompatibleWith(...AppComponent.ARMOR_ENCHANTS),
+    new Tool('leggings')
+      .addCompatibleWith(...AppComponent.ARMOR_MATERIALS)
+      .addCompatibleWith(...AppComponent.ARMOR_ENCHANTS),
+    new Tool('boots')
+      .addCompatibleWith(...AppComponent.ARMOR_MATERIALS)
+      .addCompatibleWith(...AppComponent.ARMOR_ENCHANTS,'feather_falling'),
   ];
   public materials: Material[] = [
     new Material('wood'),
@@ -37,7 +51,13 @@ export class AppComponent implements OnInit {
     new Enchant('looting', 1, 256),
     new Enchant('blast_protection', 1, 256),
     new Enchant('fire_protection', 1, 256),
+    new Enchant('protection', 1, 256),
+    new Enchant('projectile_protection', 1, 256),
     new Enchant('thorns', 1, 256),
+    new Enchant('respiration', 1, 256),
+    new Enchant('feather_falling', 1, 256),
+    new Enchant('fire_aspect', 1, 256),
+    new Enchant('knockback', 1, 256),
   ];
 
   ngOnInit(): void {
--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -1,44 +1,62 @@
 export abstract class Thing {
   id: string = '';
-  compatible: string[]|null=null;
-  constructor(id:string) {
-    this.id=id;
+  compatible: string[] | null = null;
+
+  constructor(id: string) {
+    this.id = id;
   }
+
   abstract getImage(): string;
-  getCompatibleWith(): string[]|null{
+
+  getCompatibleWith(): string[] | null {
     return this.compatible;
   }
-  setCompatibleWith(ids:string[]):Thing{
-    this.compatible=ids;
+
+  setCompatibleWith(ids: string[]): Thing {
+    this.compatible = ids;
+    return this;
+  }
+
+  addCompatibleWith(...ids: string[]): Thing {
+    if (this.compatible == null) {
+      this.compatible = [];
+    }
+    this.compatible.push(...ids);
     return this;
   }
 }
+
 export interface Gauge {
-  min:number;
-  max:number;
-  actual:number|null;
+  min: number;
+  max: number;
+  actual: number | null;
 }
+
 export class Tool extends Thing {
   getImage(): string {
     return 'assets/tools/' + this.id + '.png';
   }
 }
+
 export class Material extends Thing {
   getImage(): string {
     return 'assets/materials/' + this.id + '.png';
   }
 }
-export class Enchant extends Thing implements Gauge{
-  min= 1;
-  actual=1;
-  max= 1;
+
+export class Enchant extends Thing implements Gauge {
+  min = 1;
+  actual = 1;
+  max = 1;
+
+  constructor(id: string, min: number, max: number) {
+    super(id);
+    this.max = max;
+    this.min = min;
+    this.actual = min;
+  }
+
   getImage(): string {
     return 'assets/eBook.gif';
-  }
-  constructor(id:string, min:number, max:number) {
-    super(id);
-    this.max=max;
-    this.min=min;
-    this.actual=min;
   }
 }
